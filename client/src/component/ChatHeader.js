b8ed5d94228e439bab7c598133fcb92f79db3e08
dakshrajput45 <dakshr050@gmail.com>
2024-04-30 00:08:39 +0530

    dash-to-home

--- client/src/component/ChatHeader.js
+++ client/src/component/ChatHeader.js
@@ -1,29 +1,28 @@
-import { useCookies } from 'react-cookie'
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 
+const ChatHeader = ({ user }) => {
+    const [cookies, , removeCookie] = useCookies(['user']);
+    const navigate = useNavigate(); // Initialize useNavigate hook
 
-const ChatHeader = ({user}) => {
-
-    const[cookies,setCookie,removeCookie]=useCookies(['user'])
-
-    
-    const logout = () =>{
-        removeCookie('UserId', cookies.UserId)
-        removeCookie('Authtoken', cookies.Authtoken)
-        window.location.reload()
-    }
+    const logout = () => {
+        removeCookie('UserId', cookies.UserId);
+        removeCookie('Authtoken', cookies.Authtoken);
+        navigate("/"); // Redirect to the home page
+        window.location.reload(); // You may not need this line
+    };
 
     return (
         <div className="chat-container-header">
-              <div className="profile">
+            <div className="profile">
                 <div className="img-container">
-                    <img src={user.url} alt={"photo of" +user.first_name}/>
+                    <img src={user.url} alt={"photo of" + user.first_name}/>
                 </div>
-
                 <h3>{user.first_name}</h3>
-              </div>
-              <i className="log-out-icon" onClick={logout}>#</i>
+            </div>
+            <i className="log-out-icon" onClick={logout}>#</i>
         </div>
+    );
+};
 
-    )
-}
 export default ChatHeader;
